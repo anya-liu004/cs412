8127b44b5912a71cf13a5f4d5946ea771303f209
Anya Liu <anyaliu@bu.edu>
2025-02-06 17:46:16 -0500

    Added all files for deployment.

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path(r'', views.quote, name="quote"),
     path(r'about', views.about, name="about"),
     path(r'show_all', views.show_all, name="show_all"),
+    path(r'quote', views.quote, name="show_all"),
 ]
