41fc6b9119ea8354a50d954fffaa798df3598488
Anya Liu <anyaliu@bu.edu>
2025-02-25 12:29:44 -0500

    working on blogs app week 6

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ShowAllView, ArticleView, RandomArticleView # our view class definition 
+from .views import * # ShowAllView, ArticleView, RandomArticleView # our view class definition 
 
 urlpatterns = [
     # map the URL (empty string) to the view
     path('', RandomArticleView.as_view(), name='random'), 
     path('show_all', ShowAllView.as_view(), name='show_all'), 
+    path('article/create', CreateArticleView.as_view(), name="create_article"), # new
     path('article/<int:pk>', ArticleView.as_view(), name='article'), # show one article ### NEW
-
+    path('article/<int:pk>/create_comment', CreateCommentView.as_view(), name='create_comment'), ### NEW
 ]
